de825af7e1a8845eed98402e48ba792219ac5918
JanoKasu <109106614+JanoKasu@users.noreply.github.com>
2024-03-18 08:46:03 -0400

    Adding Git

---
+++ GraphVisualization.py
@@ -0,0 +1,52 @@
+# First networkx library is imported 
+# along with matplotlib 
+import networkx as nx 
+import matplotlib.pyplot as plt 
+
+
+# Defining a Class 
+class GraphVisualization: 
+
+	def __init__(self): 
+		
+		# visual is a list which stores all 
+		# the set of edges that constitutes a 
+		# graph 
+		self.visual = [] 
+		
+	# addEdge function inputs the vertices of an 
+	# edge and appends it to the visual list 
+	def addEdge(self, a, b, w): 
+		temp = [a, b, w]
+		self.visual.append(temp) 
+		
+	# In visualize function G is an object of 
+	# class Graph given by networkx G.add_edges_from(visual) 
+	# creates a graph with a given list 
+	# nx.draw_networkx(G) - plots the graph 
+	# plt.show() - displays the graph 
+	def visualize(self): 
+		G = nx.Graph()
+		G.add_weighted_edges_from(self.visual)
+		edge_labels = nx.get_edge_attributes(G, "weight")
+		pos = nx.spiral_layout(G)
+
+		# Draw the nodes
+		nx.draw_networkx_nodes(G, pos, node_size=250)
+
+		# Draw the edges
+		nx.draw_networkx_edges(G, pos, width=1)
+
+		# Draw the labels
+		# node labels
+		nx.draw_networkx_labels(G, pos, font_size=10, font_family="sans-serif")
+		# edge weight labels
+		edge_labels = nx.get_edge_attributes(G, "weight")
+		nx.draw_networkx_edge_labels(G, pos, edge_labels)
+
+		# Show
+		ax = plt.gca()
+		ax.margins(0.08)
+		plt.axis("off")
+		plt.tight_layout()
+		plt.show()
---
+++ Reader.py
@@ -0,0 +1,67 @@
+import json
+import PyPDF2
+import re
+import statistics
+import numpy as np
+from collections import OrderedDict, defaultdict
+from typing import Dict
+
+##################################################
+
+# Takes the full dictionary of a grams as an argument
+def sort(grams):
+    keys = list(grams.keys())
+    values = list(grams.values())
+    sorted_value_index = np.argsort(values)
+    grams = {keys[i]: values[i] for i in sorted_value_index}
+    return grams
+
+##################################################
+
+def readPDForTxt(pdf):
+    # Get the number of times a monogram, digram, or trigram appears
+    monogram = defaultdict()
+    digram = defaultdict()
+
+    # This loop is for the pages
+    pdfReader = PyPDF2.PdfReader(pdf)
+
+    for i in range(1, len(pdfReader.pages)):
+        page = pdfReader.pages[i]
+        words = page.extract_text().lower()
+        # Remove non-ASCII characters from the text
+        words = re.sub(r'[^\x61-\x7B,./;]', '', words).split()
+        
+        # This loop is for the words
+        for j in range(len(words)):
+            # This loop is for sets of 1 letter (monograms)
+            for k in range(len(words[j])):
+                di = words[j][k]
+                if monogram.__contains__(di):
+                    monogram[di] = monogram.get(di) + 1
+                else:
+                    monogram[di] = 1
+            
+            # This loop is for sets of 2 letters (digrams)
+            for k in range(len(words[j])-1):
+                di = words[j][k] + words[j][k+1]
+                rev = words[j][k+1] + words[j][k]
+                if digram.__contains__(rev):
+                    digram[rev] = digram.get(rev) + 1
+                elif digram.__contains__(di):
+                    digram[di] = digram.get(di) + 1
+                else:
+                    digram[di] = 1
+
+    ##################################################
+
+    # Sort the dictionaries by value (small -> large)
+
+    monogram = sort(monogram)
+    digram = sort(digram)
+
+    # Store the data in a json file
+    with open("data.json", "w") as outfile:
+        grams = { 'monogram' : monogram, 'digram' : digram }
+        json.dump(grams, outfile, indent=4 )
+        
